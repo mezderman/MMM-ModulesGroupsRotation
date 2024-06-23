e43b626c55374ac20e0bb466e30de9b82a5818f7
sam detweiler <sdetweil@gmail.com>
2024-06-23 16:05:28 -0500

    add groupTimeout list for different times for different groups, and MMM-Config schema

--- MMM-ModulesGroupsRotation.js
+++ MMM-ModulesGroupsRotation.js
@@ -1,31 +1,51 @@
 Module.register("MMM-ModulesGroupsRotation", {
     // Module config defaults.
     defaults: {
-        modulesGroups: [],
+        modulesGroups: [["string"]],
         rotationTime: 3000,
-        animationDelay: 1000 // Animation delay in milliseconds
+        animationDelay: 1000,// Animation delay in milliseconds
+        groupDelay:[{groupNumber:1,timeout:10000}]
     },
-
+    // used to calc delay for individual groups
+    timeout_delay:0,
     // Define start sequence.
     start() {
         Log.log(`Page start timer for rotating modules`);
-        this.curGroup = 0;
-        this.timer = setInterval(() => {
-            this.rotateModules();
-        }, this.config.rotationTime);
+        this.curGroup = -1;
+        // if no separate group delays
+        if(this.groupDelay.length==0){
+            // do it like before
+            this.timer = setInterval(() => {
+                this.rotateModules();
+            }, this.config.rotationTime);
+        }
     },
 
     rotateModules: function () {
+        // Hide all modules first with a transition
+        this.hideAllModules();
+
         this.curGroup = (this.curGroup + 1) % this.config.modulesGroups.length;
         Log.log(`this.curGroup:`, this.curGroup);
 
-        // Hide all modules first with a transition
-       this.hideAllModules();
-
+        this.timeout_delay=this.config.rotationTime
+        for(group of groupDelay){
+            if((group.groupNumber>0) && ((group.groupNumber-1) == this.curGroup)){
+                this.timeout_delay=group.timeout
+                break;
+            }
+        }
+        // fire quickly on startup
+        let animationDelay= this.curGroup == 0?1:this.config.animationDelay
         // Show modules in the current group after a short delay to allow hide transition to complete
         setTimeout(() => {
             this.showCurrentGroup()
-        }, this.config.animationDelay); 
+            if(this.groupDelay.length){
+                this.timer = setTimeout(() => {
+                    this.rotateModules();
+                }, this.timeout_delay);
+            }
+        }, animationDelay);
     },
 
     fadeOut(module) {
@@ -61,8 +81,7 @@ Module.register("MMM-ModulesGroupsRotation", {
     // Override dom generator.
     getDom() {
         const wrapper = document.createElement("div");
-        this.hideAllModules();
-        this.showCurrentGroup();
+        this.rotateModules();
         return wrapper;
     },
 
